22de90acbb81cc62cfac1aea5831d2e76494e9a2
Karl robeck alferez <karlalferezfx@gmail.com>
2025-11-08 03:35:24 +0800

    feat: Add handle method to MayaWebhookService for request handling

--- src/services/webhook.ts
+++ src/services/webhook.ts
@@ -1,7 +1,11 @@
 export class MayaWebhookService {
+	//
 	createWebhook() {}
 	getWebhooks() {}
 	getWebhook() {}
 	updateWebhook() {}
 	deleteWebhook() {}
+	handle(request: Request): Response {
+		throw new Error("");
+	}
 }
